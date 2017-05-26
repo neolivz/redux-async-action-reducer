a043d7e825e53c2c40d8f5c06245e566f1d4bfef
Jishnu Viswanath <github@jishnu.me>
2017-05-26 13:48:02 +1000

    Unit Test Actions

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./actions"));
-__export(require("./reducers"));
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiL1VzZXJzL25lb2xpdnovV29yay9SbmQvcmVkdXgtdGh1bmstYWN0aW9uLXJlZHVjZXIvbGliLyIsInNvdXJjZXMiOlsiaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSwrQkFBMEI7QUFDMUIsZ0NBQTJCIn0=
---
+++ lib/tests/actions.spec.ts
@@ -0,0 +1,62 @@
+import actions from '../actions'
+import { SimpleActionCreatorResponse, AsyncActionCreatorReponse, ApiFunc } from '../actions'
+
+const { createAsyncAction, createSimpleAction } = actions;
+
+//actions.createSimpleAction
+describe( 'Create Simple Action', () => {
+    let simpleNone: SimpleActionCreatorResponse<string, undefined>;
+    let simpleString: SimpleActionCreatorResponse<string, string>;
+    let simpleNumber: SimpleActionCreatorResponse<string, number>;
+    let simpleObject: SimpleActionCreatorResponse<string, object>;
+    beforeEach(() => {
+        simpleNone = createSimpleAction<string, undefined>( 'TOGGLE' );
+        simpleString = createSimpleAction<string, string>( 'CREATE' );
+        simpleNumber = createSimpleAction<string, number>( 'NUMBER' );
+        simpleObject = createSimpleAction<string, object>( 'OBJECT' );
+    } )
+
+    it( 'should create simple action', () => {
+        expect( simpleNone() ).toEqual( { type: 'TOGGLE' } );
+        expect( simpleString( 'A' ) ).toEqual( { type: 'CREATE', request: 'A' } );
+        expect( simpleNumber( 10 ) ).toEqual( { type: 'NUMBER', request: 10 } );
+        expect( simpleObject( { 'test': 'test' } ) ).toEqual( { type: 'OBJECT', request: { 'test': 'test' } } );
+    } );
+} );
+
+describe( 'Create Async Action: SUCCESS', () => {
+
+    let api: ApiFunc<any, any> = jest.fn(() => Promise.resolve() ),
+        dispatch = jest.fn();
+
+    let asyncString: AsyncActionCreatorReponse<string, string, string> = createAsyncAction<string, string, string>( 'CREATE', api );
+
+    beforeEach(() => {
+        asyncString()( dispatch )
+    } )
+
+    it( 'should create async action', () => {
+        expect( api ).toHaveBeenCalled()
+        expect( dispatch.mock.calls.length ).toEqual( 2 )
+        expect( dispatch.mock.calls[ 0 ] ).toEqual( [ { "request": undefined, "status": "STARTED", "type": "CREATE" }] )
+        expect( dispatch.mock.calls[ 1 ] ).toEqual( [ { "request": undefined, "status": "SUCCESS", "type": "CREATE" }] )
+    } );
+} );
+describe( 'Create Async Action: FAILURE', () => {
+    const error = { error: 'Error' };
+    let api: ApiFunc<any, any> = jest.fn(() => Promise.reject( error ) ),
+        dispatch = jest.fn();
+
+    let asyncString: AsyncActionCreatorReponse<string, string, string> = createAsyncAction<string, string, string>( 'CREATE', api );
+
+    beforeEach(() => {
+        asyncString()( dispatch )
+    } )
+
+    it( 'should create async action', () => {
+        expect( api ).toHaveBeenCalled()
+        expect( dispatch.mock.calls.length ).toEqual( 2 )
+        expect( dispatch.mock.calls[ 0 ] ).toEqual( [ { "request": undefined, "status": "STARTED", "type": "CREATE" }] )
+        expect( dispatch.mock.calls[ 1 ] ).toEqual( [ { "request": undefined, "status": "FAILURE", "type": "CREATE", "error": error }] )
+    } );
+} );
